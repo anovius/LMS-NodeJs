93c60227586bd3166ae5c08a4a7b9c61b10ed0fd
Usman Farooq <anovius10@gmail.com>
2021-06-28 02:12:40 +0500

    final before production

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const Router = express.Router()
+const path = require('path');
+
+Router.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd()+'/public/index.html'))
+})
 
 Router.use('/api', require('./api'))
+
 
 module.exports = Router
