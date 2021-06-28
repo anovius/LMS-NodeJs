69f799d142c82d724398a5a676968742c03f5b3e
Usman Farooq <anovius10@gmail.com>
2021-06-28 17:26:53 +0500

    Shell Scripts Added

---
+++ scripts/index.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bash node.sh;
+bash mongodb.sh;
+bash nginx.sh;
+bash jenkins.sh
---
+++ scripts/node.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt install nodejs -y;
+sudo apt install npm;
+sudo npm install pm2 -g;
